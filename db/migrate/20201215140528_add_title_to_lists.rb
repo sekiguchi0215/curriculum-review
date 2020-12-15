d50881378ce6948614dc36034fea189e9f39f3fa
vagrant <vagrant@localhost.localdomain>
2020-12-15 14:13:29 +0000

    [Finish] Rails chapter5

---
+++ db/migrate/20201215140528_add_title_to_lists.rb
@@ -0,0 +1,6 @@
+class AddTitleToLists < ActiveRecord::Migration[5.2]
+  def change
+    add_column :lists, :title, :string
+    add_column :lists, :body,  :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_134923) do
+ActiveRecord::Schema.define(version: 2020_12_15_140528) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
