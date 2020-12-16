5d03482457205eee4371c284baae1a0b07fb7e8d
vagrant <dsphere5296@gmail.com>
2020-12-16 14:15:27 +0000

    [Finish] Rails chapter8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b6dc1497983d6eab7f9a940595810c2ead6c302568638e9071c1dcc13a7406f9997fd70e8d57261bceb35a71b1e302df0e8b1d613a89ab08a5c95315a27cbae0'
