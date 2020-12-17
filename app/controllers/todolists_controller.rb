4ea71f692340bd2e85170bdecc90b90015a8a6ec
vagrant <dsphere5296@gmail.com>
2020-12-17 09:24:05 +0000

    [Finish] Rails chapter9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -30,7 +30,7 @@ class TodolistsController < ApplicationController
   def destroy
   	list = List.find(params[:id])
   	list.destroy
-  	redirect_to todolist_path
+  	redirect_to todolists_path
   end
 
   private
